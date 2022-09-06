2cf35e217a1619a6b7fbd982ebba2949693823d1
WilgotW <89266933+WilgotW@users.noreply.github.com>
2022-09-06 17:16:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,38 @@
+const canvas = document.getElementById("canvas1");
+const c = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 let monthsLeft;
 let daysLeft;
 let hoursLeft;
 let minutesLeft;
 let secondsLeft;
 
+let particles = [];
+let hue = 0;
+let hsl = "";
+let maxParticleInScene = 10000;
+let colorChangeSpeed = 1;
+let particleAmount = 10;
+let screenRefrechOpacity = '1';
+let linesOn = true;
+let e;
+
 const monthNum = document.getElementById("months")
 const daysNum = document.getElementById("days");
 const hoursNum = document.getElementById("hours");
 const minutesNum = document.getElementById("minutes");
 const secondsNum = document.getElementById("seconds");
-
-function updateDate(){
-    //months:
-    monthNum.innerHTML = monthsLeft;
-    //days:
-    daysNum.innerHTML = daysLeft;
-    //hours:
-    hoursNum.innerHTML = hoursLeft;
-    //minutes:
-    minutesNum.innerHTML = minutesLeft;
-    //seconds:
-    secondsNum.innerHTML = secondsLeft;
-
-    requestAnimationFrame(updateDate);
-}
-updateDate();
-
 const getDays = (year, month) => {
     return new Date(year, month, 0).getDate();
 };
-
-function countDown(){
-    //months
-    monthsLeft = 12 - (new Date().getMonth() + 1);
-    //days:
-    let daysThisMonth = getDays(new Date().getFullYear(), (new Date().getMonth() + 1));
-    daysLeft = daysThisMonth - new Date().getDate();
-    //hours:
-    hoursLeft = 24 - new Date().getHours();
-    //minutes:
-    minutesLeft = 60 - new Date().getMinutes();
-    //seconds:
-    secondsLeft = 60 - new Date().getSeconds();
-
-    requestAnimationFrame(countDown);
-}
-countDown();
-
-
-
-const canvas = document.getElementById("canvas1");
-const c = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let particles = [];
-let hue = 0;
-let hsl = "";
-
-let maxParticleInScene = 10000;
-let colorChangeSpeed = 1;
-let particleAmount = 10;
-let screenRefrechOpacity = '1';
-
-let linesOn = true;
-
-let e;
-
 const mouse = {
     x: undefined,
     y: undefined
 };
-
+let currentMouseX = mouse.x;
+let currentMouseY = mouse.y;
 
 class Particle{
     constructor(x, y, color){
@@ -97,6 +58,37 @@ class Particle{
             this.radius -= this.shrinkRate;
         }
     }
+}
+function updateDate(){
+    //months:
+    monthNum.innerHTML = monthsLeft;
+    //days:
+    daysNum.innerHTML = daysLeft;
+    //hours:
+    hoursNum.innerHTML = hoursLeft;
+    //minutes:
+    minutesNum.innerHTML = minutesLeft;
+    //seconds:
+    secondsNum.innerHTML = secondsLeft;
+
+    requestAnimationFrame(updateDate);
+}
+
+
+function countDown(){
+    //months
+    monthsLeft = 12 - (new Date().getMonth() + 1);
+    //days:
+    let daysThisMonth = getDays(new Date().getFullYear(), (new Date().getMonth() + 1));
+    daysLeft = daysThisMonth - new Date().getDate();
+    //hours:
+    hoursLeft = 24 - new Date().getHours();
+    //minutes:
+    minutesLeft = 60 - new Date().getMinutes();
+    //seconds:
+    secondsLeft = 60 - new Date().getSeconds();
+
+    requestAnimationFrame(countDown);
 }
 
 function update(){
@@ -126,36 +118,8 @@ function update(){
     
     requestAnimationFrame(update);
 }
-update();
 
 
-window.addEventListener('resize', function(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-});
-
-window.addEventListener('mousemove', function(event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-    
-    for(i = 0; i < particleAmount; i++){
-        particles.push(new Particle(mouse.x, mouse.y, hsl));
-    }
-    
-});
-
-window.addEventListener('click', function(event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-    
-    for(i = 0; i < 50; i++){
-        particles.push(new Particle(mouse.x, mouse.y, hsl));
-    }
-    
-});
-
-let currentMouseX = mouse.x;
-let currentMouseY = mouse.y;
 function checkInactivity(){
     if(mouse.x == currentMouseX && mouse.y == currentMouseY){
         removePaint();
@@ -163,8 +127,6 @@ function checkInactivity(){
     currentMouseX = mouse.x;
     currentMouseY = mouse.y;
 }
-setInterval(checkInactivity, 1000);
-
 function refrech(){
     c.fillStyle = 'rgba(0, 0, 0, 0.1)';
     c.fillRect(0, 0, canvas.width, canvas.height);
@@ -174,3 +136,29 @@ function removePaint(){
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
 }
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+window.addEventListener('mousemove', function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    for(i = 0; i < particleAmount; i++){
+        particles.push(new Particle(mouse.x, mouse.y, hsl));
+    }
+});
+
+window.addEventListener('click', function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    for(i = 0; i < 50; i++){
+        particles.push(new Particle(mouse.x, mouse.y, hsl));
+    }
+});
+
+
+updateDate();
+update();
+countDown();
+setInterval(checkInactivity, 1000);
